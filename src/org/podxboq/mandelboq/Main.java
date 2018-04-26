a0bfed6b8599cdbadcfd0abec3c78b072193f7b2
podxboq <podxboq@gmail.com>
2018-04-27 01:16:11 +0200

    Paso del canvas al plano complejo

--- src/org/podxboq/mandelboq/Main.java
+++ src/org/podxboq/mandelboq/Main.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.commons.math3.complex.Complex;
+import org.podxboq.mandelboq.maths.AfinT;
 import org.podxboq.mandelboq.maths.Plano;
 import org.podxboq.mandelboq.ui.MainMenu;
 
@@ -31,7 +32,8 @@ public class Main extends Application {
 		primaryStage.show();
 
 		Platform.runLater(new Runnable() {
-			@Override public void run() {
+			@Override
+			public void run() {
 				test(canvas);
 			}
 		});
@@ -42,10 +44,17 @@ public class Main extends Application {
 		p.setVisible(new Complex(-1, -1), new Complex(1, 1));
 		p.updateCanvas(c);
 		//p.render();
+		AfinT phi = new AfinT();
+		phi.setView(c.getWidth(), c.getHeight());
+		phi.setPlano(new Complex(-2, -2), new Complex(2, 2));
 		final PixelWriter pixelWriter = c.getGraphicsContext2D().getPixelWriter();
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-				pixelWriter.setColor(i, j, Color.BLACK);
+		for (int i = 0; i < c.getWidth(); i++) {
+			for (int j = 0; j < c.getHeight(); j++) {
+				Complex z = phi.img(new Complex(i, j));
+				if (z.abs() < 2) {
+					pixelWriter.setColor(i, j, Color.BLACK);
+				}
+				System.out.println(z.getReal() + "+i " + z.getImaginary());
 			}
 		}
 	}
---
+++ src/org/podxboq/mandelboq/maths/AfinT.java
@@ -0,0 +1,33 @@
+package org.podxboq.mandelboq.maths;
+
+import org.apache.commons.math3.complex.Complex;
+
+public class AfinT {
+
+	private double w; //width of canvas
+	private double h; //height of canvas
+	private Complex d1; //punto inferior de la diagonal de la parte visible del plano
+	private Complex d2; //punto superior de la diagonal de la parte visible del plano
+
+	public void setView(double w, double h) {
+		this.w = w;
+		this.h = h;
+	}
+
+	public void setPlano(Complex d1, Complex d2) {
+		this.d1 = d1;
+		this.d2 = d2;
+	}
+
+	public Complex img(Complex z) {
+		double x = z.getReal();
+		double y = z.getImaginary();
+		double a = d1.getReal();
+		double b = d1.getImaginary();
+		double c = d2.getReal();
+		double d = d2.getImaginary();
+		double imgX = (c - a) / w * x + a;
+		double imgY = (b - d) / h * y + d;
+		return new Complex(imgX, imgY);
+	}
+}
